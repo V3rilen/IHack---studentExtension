5dc0afb9ffd742a23403d30e90a05fd44f6b87c1
V3rilen <asher@verilen.com>
2023-10-21 04:34:16 -0600

    stuff and thing

--- background.js
+++ background.js
@@ -1,10 +1,25 @@
+highlightID = "highlightText";
 chrome.contextMenus.create({
-  id: "highlight_text",
+  id: "highlightText",
   title: "Highlight Text",
   contexts: ["selection"],
 });
+
+//context menu option to look up rate my professor
+rateID = "rateProfessors";
+chrome.contextMenus.create({
+  id: rateID,
+  title: "Look up professor rating",
+  contexts: ["selection"],
+});
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId === "highlight_text") {
+  if (info.menuItemId == rateID) {
+    const selectedText = info.selectionText;
+    console.log(selectedText);
+    const searchURL =
+      "https://www.ratemyprofessors.com/search/professors?q=" + selectedText;
+    chrome.tabs.create({ url: searchURL });
+  } else if (info.menuItemId === "highlightText") {
     const selectedText = info.selectionText;
     console.log(info);
     if (selectedText) {
@@ -17,25 +32,9 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
         lastFocusedWindow: true,
       });
       chrome.tabs.sendMessage(tab.id, {
-        message_id: "highlight_text",
+        message_id: "highlightText",
         data: selectedText,
       });
     }
-  }
-});
-
-//context menu option to look up rate my professor
-rateID = "rateProfessors";
-chrome.contextMenus.create({
-  id: rateID,
-  title: "Look up professor rating",
-  contexts: ["selection"]
-});
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId == rateID) {
-    const selectedText = info.selectionText;
-    console.log(selectedText);
-    const searchURL = "https://www.ratemyprofessors.com/search/professors?q=" + selectedText;
-    chrome.tabs.create({ url: searchURL});
   }
 });
--- content.js
+++ content.js
@@ -1,41 +1,122 @@
-(() => {
+const fetchHighlights = (url) => {
+  return chrome.storage.local.get([url]);
+};
+
+function getXPathOfElement(element) {
+  if (element.id !== "") {
+    // If the element has an ID, use it for the XPath
+    return `//*[@id="${element.id}"]`;
+  }
+
+  if (element === document.body) {
+    // If we reach the body element, stop
+    return element.tagName;
+  }
+
+  // Find the element's index among its siblings
+  let index = 1;
+  let sibling = element;
+
+  while (sibling && (sibling = sibling.previousElementSibling)) {
+    index++;
+  }
+
+  // Recurse up the DOM to build the full XPath
+  return (
+    getXPathOfElement(element.parentNode) + "/" + element.tagName + `[${index}]`
+  );
+}
+
+function createElementAtXPath(xpath, newElement) {
+  const result = document.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.ANY_UNORDERED_NODE_TYPE,
+    null
+  );
+
+  const targetElement = result.singleNodeValue;
+
+  if (targetElement) {
+    const element = document.createElement(newElement.tagName);
+    element.textContent = newElement.textContent;
+
+    targetElement.parentNode.insertBefore(element, targetElement);
+  } else {
+    console.log("error");
+  }
+}
+
+// Example usage:
+// const targetXPath = createXPath(document.getElementById("targetElement"));
+// const newElementInfo = {
+//   tagName: "p",
+//   textContent: "This is a new paragraph.",
+// };
+
+// createElementAtXPath(targetXPath, newElementInfo);
+
+(async () => {
   console.log("context.js loaded");
-  chrome.runtime.onMessage.addListener(function (
-    request,
-    sender,
-    sendResponse
-  ) {
-    if (request.message_id == "highlight_text") {
-      const selectedText = window.getSelection();
-      if (selectedText.toString()) {
-        let newSpan = document.createElement("span");
-        newSpan.textContent = selectedText;
-        // User has selected text, you can perform actions here
-        let parentNode = selectedText.anchorNode.parentNode;
-        // let containingNode = parentNode.childNodes.find((child) =>
-        //   child.data.includes(selectedText.toString())
-        // );
-        console.log(parentNode.childNodes);
-        // if(parentNode.getComputedStyle()){
+  const currentURL = window.location.toString();
 
-        // }
-        let new_text = parentNode.textContent.split(selectedText.toString());
-        if (selectedText) {
-          // console.log("Split text", new_text);
-          new_text.splice(
-            1,
-            0,
-            `<span class="highlighted">${selectedText.toString()}</span>`
-          );
-          new_text.splice(1, 0, `${selectedText.toString()}`);
-          parentNode.innerHTML = new_text.join("");
-          console.log("Selected Text:", new_text);
+  let pageHighlights = [];
+  if (await fetchHighlights(currentURL)) {
+    pageHighlights = await fetchHighlights(currentURL);
+    // console.log(await fetchHighlights(currentURL));
+  }
+  //   console.log(pageHighlights);
+
+  chrome.runtime.onMessage.addListener(
+    async (request, sender, sendResponse) => {
+      if (request.message_id == "highlightText") {
+        const selectedText = window.getSelection();
+        if (selectedText.toString()) {
+          let selectedTextRange = selectedText.getRangeAt(0);
+          if (
+            selectedText.anchorNode.parentNode.classList.contains("highlighted")
+          ) {
+            selectedText.anchorNode.parentNode.classList.toggle("highlighted");
+          } else {
+            const newHighlightedElement = document.createElement("span");
+            newHighlightedElement.className = "highlighted";
+            selectedTextRange.surroundContents(newHighlightedElement);
+            let highlightData = {
+              xpath: getXPathOfElement(newHighlightedElement),
+              textContent: newHighlightedElement.textContent,
+            };
+            console.log(highlightData);
+            pageHighlights.push(highlightData);
+
+            //   console.log(getElementXPath(newHighlightedElement.parentElement));
+            //   const targetXPath = getElementXPath(
+            //     newHighlightedElement.parentElement
+            //   );
+            //   const newElementInfo = {
+            //     tagName: "span",
+            //     textContent: "WOOOOOOOORDS",
+            //   };
+
+            //   createElementAtXPath(targetXPath, newElementInfo);
+            //   //   const rangeData = {
+            //   //     commonAncestorContainer: selectedTextRange.commonAncestorContainer,
+            //   //     startContainer: selectedTextRange.startContainer,
+            //   //     startOffset: selectedTextRange.startOffset,
+            //   //     endContainer: selectedTextRange.endContainer,
+            //   //     endOffset: selectedTextRange.endOffset,
+            //   //   };
+            //   pageHighlights.push(getElementXPath(newHighlightedElement));
+            //   console.log("page highlights before:", selectedText);
+            //   console.log(document.documentElement.toString());
+            chrome.storage.local.set({
+              [currentURL]: pageHighlights,
+            });
+            console.log();
+          }
         }
-
-        // Send a message to the background script
-        // chrome.runtime.sendMessage({ selectedText });
       }
+      /* Content script action */
     }
-    /* Content script action */
-  });
+  );
 })();
