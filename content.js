f9ec168804d51c1c452b8e0f255f29f157e9f801
V3rilen <asher@verilen.com>
2023-10-21 00:06:40 -0600

    update to content.js

--- content.js
+++ content.js
@@ -7,18 +7,27 @@
   ) {
     if (request.message_id == "highlight_text") {
       const selectedText = window.getSelection();
-      if (selectedText) {
+      if (selectedText.toString()) {
+        let newSpan = document.createElement("span");
+        newSpan.textContent = selectedText;
         // User has selected text, you can perform actions here
         let parentNode = selectedText.anchorNode.parentNode;
+        // let containingNode = parentNode.childNodes.find((child) =>
+        //   child.data.includes(selectedText.toString())
+        // );
+        console.log(parentNode.childNodes);
+        // if(parentNode.getComputedStyle()){
+
+        // }
         let new_text = parentNode.textContent.split(selectedText.toString());
-        if (selectedText.toString()) {
+        if (selectedText) {
           // console.log("Split text", new_text);
           new_text.splice(
             1,
             0,
-            `<span style="background-color:yellow;">${selectedText.toString()}</span>`
+            `<span class="highlighted">${selectedText.toString()}</span>`
           );
-          // new_text.splice(1, 0, `${selectedText.toString()}`);
+          new_text.splice(1, 0, `${selectedText.toString()}`);
           parentNode.innerHTML = new_text.join("");
           console.log("Selected Text:", new_text);
         }
