7660ed0eb7b7cadca47e9900ed25f43a6b533ce6
Eyeman362 <eye.sack.00000@gmail.com>
2023-10-21 02:20:05 -0600

    x

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('activateButton').addEventListener('click', function () {
-      // Perform an action when the "Activate" button is clicked
-      // You can add your functionality here
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const timerInput = document.getElementById("timerInput");
+  const startTimerButton = document.getElementById("startTimer");
+  const timerDisplay = document.getElementById("timerDisplay");
+
+  chrome.storage.sync.get(["timerValue"], function (data) {
+    if (data.timerValue) {
+      timerInput.value = data.timerValue;
+    }
   });
-  
+
+  startTimerButton.addEventListener("click", function () {
+    const timeInSeconds = parseInt(timerInput.value);
+
+    if (!isNaN(timeInSeconds) && timeInSeconds > 0) {
+      chrome.storage.sync.set({ "timerValue": timeInSeconds });
+      timerDisplay.innerText = `Timer: ${timeInSeconds} seconds`;
+
+      // Start your timer logic here using the timeInSeconds value
+    } else {
+      timerDisplay.innerText = "Invalid input";
+    }
+  });
+});
